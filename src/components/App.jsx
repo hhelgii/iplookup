dd16f3347b57c76c95151352b5b1e9f1824069b3
hhelgii <opruska2004@gmail.com>
2023-09-04 01:36:22 +0300

    styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
 
+
+import React, { useState } from 'react';
 import { Form } from './form/form';
 import { fetchInfo } from 'services/api';
 import apiInfo from 'services/apiInfo';
-import styles from './app.module.css'
+import styles from './app.module.css';
+
 export const App = () => {
   const [inputIpValue, setInputIpValue] = useState('');
   const [result, setResult] = useState(null);
   const [error, setError] = useState('');
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     setInputIpValue(event.target.value);
   };
 
-  const handleIpLookup = async event => {
+  const handleIpLookup = async (event) => {
     event.preventDefault();
     if (!isValidIp(inputIpValue)) {
       setError('Invalid IP address');
-      setResult(null)
+      setResult(null);
       return;
     }
 
@@ -31,30 +33,38 @@ export const App = () => {
     }
   };
 
-  const isValidIp = ip => {
+  const isValidIp = (ip) => {
     const pattern =
       /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     return pattern.test(ip);
   };
 
   return (
-    <>
-      <Form
-        inputValue={inputIpValue}
-        onInputChange={handleInputChange}
-        onIpLookup={handleIpLookup}
-      />
-      {result && (
-      <div className={styles.resultInfo}>
-        <p>IP Address: {result.address}</p>
-        <p>Country: {result.country}</p>
-        <p>City: {result.city}</p>
-        <p>Latitude: {result.latitude}</p>
-        <p>Longitude: {result.longitude}</p>
-        <p>Timezone: {result.timezone}</p>
+    <div className={styles.container}>
+      <h1>Enter the IP address and click the button!</h1>
+      <div className={styles.appContainer}>
+        
+        <div className={styles.leftContainer}>
+          <Form
+            inputValue={inputIpValue}
+            onInputChange={handleInputChange}
+            onIpLookup={handleIpLookup}
+          />
+        </div>
+        <div className={styles.rightContainer}>
+          {result && (
+            <div className={styles.resultInfo}>
+              <p>IP Address: {result.address}</p>
+              <p>Country: {result.country}</p>
+              <p>City: {result.city}</p>
+              <p>Latitude: {result.latitude}</p>
+              <p>Longitude: {result.longitude}</p>
+              <p>Timezone: {result.timezone}</p>
+            </div>
+          )}
+          {error && <p className={styles.errorMessage}>Error: {error}</p>}
+        </div>
       </div>
-    )}
-    {error && <p className={styles.errorMessage}>Error: {error}</p>}
-    </>
+    </div>
   );
 };
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,17 +1,25 @@
+
+
 import React from 'react';
-import styles from './form.module.css'; 
+import styles from './form.module.css';
 
 export const Form = ({ inputValue, onInputChange, onIpLookup }) => {
   return (
-    <form className={styles.form}>
-      <input
-        type="text"
-        placeholder="Enter IP address"
-        value={inputValue}
-        onChange={onInputChange}
-        className={styles.input} 
-      />
-      <button onClick={onIpLookup} className={styles.button}>IP Lookup</button> 
+    <form className={styles.formContainer}>
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          placeholder="Enter IP address"
+          value={inputValue}
+          onChange={onInputChange}
+          className={styles.inputField}
+          minLength={8}
+          maxLength={16}
+        />
+        <button onClick={onIpLookup} className={styles.button}>
+          IP Lookup
+        </button>
+      </div>
     </form>
   );
 };
